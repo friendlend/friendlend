983a01a78d73e5c03f10a6334b33d761f4c9c455
Michael <checomichael2@gmail.com>
2019-07-01 12:36:34 -0400

    added routing

--- app/src/authenticated-app.js
+++ app/src/authenticated-app.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import { Router, Redirect } from '@reach/router';
 const AuthenticatedApp = () => {
-	return <div>Authenticated!</div>;
+	return (
+		<div>
+			<p>Authenticated!</p>
+			<Routes />
+		</div>
+	);
 };
 
+function Routes() {
+	return (
+		<Router>
+			{/* <Redirector path="/" /> */}
+			<Placeholder path="/" />
+			<NotFound default />
+		</Router>
+	);
+}
+
+function Placeholder() {
+	return <p>Placeholder Page</p>;
+}
+function NotFound() {
+	return <p>Not found</p>;
+}
+function Redirector() {
+	return <Redirect to="/something" />;
+}
 export default AuthenticatedApp;
