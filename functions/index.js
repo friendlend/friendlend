1d21e7a5a41b06c3d785a010e4897455e4ecbfcc
keveightysev <keveightysev@gmail.com>
2019-07-01 17:32:30 -0500

    createStripeCustomerId callable function complete

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+const stripe = require('stripe')(functions.config().stripe.secret);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createStripeCustomer = functions.https.onCall(async data => {
+  try {
+    const customer = await stripe.customers.create({
+      email: data.email,
+      description: `Customer for user id ${data.id}`,
+    });
+    await db.doc(`users/${data.id}`).update({ stripeId: customer.id });
+    return {
+      success: true,
+      stripeId: customer.id,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      success: false,
+      error: err,
+    };
+  }
+});
