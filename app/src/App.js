690cbfbd7c73c6e07d37d5545d4a68f08d61031c
keveightysev <keveightysev@gmail.com>
2019-07-02 18:43:14 -0500

    installed linter

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,9 @@ import AuthenticatedApp from './authenticated-app';
 import UnAuthenticatedApp from './unauthenticated-app';
 import { useUser } from './context/auth-context';
 function App() {
-	const { user } = useUser();
-	console.log(user, 'user');
-	return user ? <AuthenticatedApp /> : <UnAuthenticatedApp />;
+  const { user } = useUser();
+  console.log(user, 'user');
+  return user ? <AuthenticatedApp /> : <UnAuthenticatedApp />;
 }
 
 export default App;
--- app/src/Dashboard.js
+++ app/src/Dashboard.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 const Container = styled.div`
-	display: grid;
-	grid-template-areas:
-		'sidebar-1 content sidebar-2'
-		'sidebar-1 content sidebar-2';
-	grid-gap: 15px;
-	grid-template-columns: 200px 1fr 200px;
-	grid-template-rows: 100px 400px 0 150px;
-	& > * {
-		border: 1px solid black;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin: 5px;
-		grid-gap: 10px;
-	}
+  display: grid;
+  grid-template-areas:
+    'sidebar-1 content sidebar-2'
+    'sidebar-1 content sidebar-2';
+  grid-gap: 15px;
+  grid-template-columns: 200px 1fr 200px;
+  grid-template-rows: 100px 400px 0 150px;
+  & > * {
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    grid-gap: 10px;
+  }
 `;
 const Section = styled.section`
-	grid-area: content;
+  grid-area: content;
 `;
 const Aside = styled.aside`
-	grid-area: sidebar-1;
+  grid-area: sidebar-1;
 `;
 const Aside2 = styled.aside`
-	grid-area: sidebar-2;
+  grid-area: sidebar-2;
 `;
 const Dashboard = () => {
-	return (
-		<Container>
-			<Aside>Sent</Aside>
-			<Section>All the Info</Section>
-			<Aside2>Received</Aside2>
-		</Container>
-	);
+  return (
+    <Container>
+      <Aside>Sent</Aside>
+      <Section>All the Info</Section>
+      <Aside2>Received</Aside2>
+    </Container>
+  );
 };
 
 export default Dashboard;
