fa88a20c9a2c49f55fb10a73828edfaebd5693cf
keveightysev <keveightysev@gmail.com>
2019-07-01 19:06:57 -0500

    payLoan function completed

--- functions/index.js
+++ functions/index.js
@@ -42,3 +42,44 @@ exports.createStripeCard = functions.https.onCall(async data => {
     };
   }
 });
+
+exports.payLoan = functions.https.onCall(async data => {
+  try {
+    await db.doc(`loans/${data.loanId}`).update({ status: 'pending_payout' });
+    const loanRef = await db.doc(`loans/${data.loanId}`).get();
+    const loan = loanRef.data();
+    const card = await stripe.customers.createSource(data.stripeId, {
+      source: data.token,
+    });
+    await db.doc(`users/${data.id}`).update({ stripeCardId: card.id });
+    const charge = await stripe.charges.create({
+      amount: loan.total,
+      currency: 'usd',
+      source: card.id,
+      description: `Loan payment for loan id ${loan.id}`,
+    });
+    const borrowerRef = await loan.borrower.get();
+    const borrower = borrowerRef.data();
+    const payout = await stripe.payouts.create({
+      amount: loan.amount,
+      currency: 'usd',
+      destination: borrower.stripeCardId,
+      method: 'instant',
+      description: `Loan payment for loan id ${loan.id}`,
+    });
+    await db.doc(`loans/${data.loanId}`).update({
+      status: 'loan_paid',
+      loanPaymentChargeId: charge.id,
+      loanPaymentPayoutId: payout.id,
+    });
+    return {
+      success: true,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      success: false,
+      error: err,
+    };
+  }
+});
