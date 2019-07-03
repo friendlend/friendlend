cb734c4e87c4f5e3d80895770f11bcae7333e887
keveightysev <keveightysev@gmail.com>
2019-07-03 02:41:51 -0500

    styled signup page

--- app/src/component/Auth/SignUp.js
+++ app/src/component/Auth/SignUp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from '@reach/router';
 import { auth, firebase } from '../../firebase';
+import styled from 'styled-components';
 
 const SignUp = ({ navigate, location }) => {
   const [signupForm, setSignupForm] = useState({
@@ -50,10 +52,11 @@ const SignUp = ({ navigate, location }) => {
   };
 
   return (
-    <>
-      <h2>SignUp Page</h2>
+    <SignUpPage>
+      <h2>Create Your FriendLend Account</h2>
       <form onSubmit={handleSignUp}>
         <input
+          type='text'
           id='displayName'
           placeholder='Your full name'
           value={signupForm.displayName}
@@ -76,8 +79,74 @@ const SignUp = ({ navigate, location }) => {
         <button type='submit'>Create Account</button>
       </form>
       <button onClick={handleSignInWithGoogle}>Sign in with Google</button>
-    </>
+      <br />
+      <br />
+      <p>
+        Already have an account? <Link to='/login'>Sign in here</Link>
+      </p>
+    </SignUpPage>
   );
 };
 
 export default SignUp;
+
+const SignUpPage = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input {
+      width: 25%;
+      height: 30px;
+      font-size: 1.8rem;
+      padding: 5px;
+      margin-bottom: 10px;
+      border: 1px solid black;
+      border-radius: 5px;
+    }
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    margin: 25px;
+  }
+
+  button {
+    cursor: pointer;
+    width: 25%;
+    height: 30px;
+    font-size: 1.8rem;
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    background: black;
+    color: white;
+    transition: all 200ms linear;
+    &:hover {
+      background: white;
+      color: black;
+    }
+  }
+
+  p {
+    font-size: 1.8rem;
+
+    a {
+      text-decoration: none;
+      transition: all 200ms linear;
+      color: green;
+      font-weight: 600;
+
+      &:hover {
+        border-bottom: 2px dotted green;
+      }
+    }
+  }
+`;
