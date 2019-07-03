73f159b1e1076a957a40924c9ca5ab0c79bf1b12
keveightysev <keveightysev@gmail.com>
2019-07-03 04:55:35 -0500

    Styled loan request page

--- app/src/component/LoanRequest.js
+++ app/src/component/LoanRequest.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import styled from 'styled-components';
 import { db } from '../firebase';
 import { useUser } from '../context/auth-context';
 
@@ -26,13 +27,16 @@ const LoanRequest = ({ loanId, navigate }) => {
   };
 
   return loan ? (
-    <>
-      <h2>{loan.borrower.displayName} is requesting a loan!</h2>
+    <ReviewLoanPage>
+      <h2>
+        &#x1F4B8; &#x1F4B8; &#x1F4B8; {loan.borrower.displayName} is requesting
+        a loan! &#x1F4B8; &#x1F4B8; &#x1F4B8;
+      </h2>
       <h3>Here are the details...</h3>
       <table>
         <tbody>
           <tr>
-            <td>Amount Requested:</td>
+            <td>Amount Requested</td>
             <td>${(loan.total / 100).toFixed(2)}</td>
           </tr>
           <tr>
@@ -45,12 +49,56 @@ const LoanRequest = ({ loanId, navigate }) => {
           </tr>
         </tbody>
       </table>
-      <h3>Can you help out?</h3>
+      <h3>&#x1F64F; Can you help out? &#x1F64F;</h3>
       <button onClick={handleAccept}>Send money</button>
-    </>
+    </ReviewLoanPage>
   ) : (
     <p>Loading...</p>
   );
 };
 
 export default LoanRequest;
+
+const ReviewLoanPage = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  h2 {
+    font-size: 2.5rem;
+    margin-bottom: 20px;
+  }
+  h3 {
+    font-size: 2rem;
+    margin-bottom: 15px;
+  }
+  table {
+    font-size: 1.8rem
+    width: 45%;
+    height: 100px;
+    margin: 10px;
+    td:first-child {
+      text-align: right;
+      padding-right: 5px;
+    }
+    td:last-child {
+      padding-left: 5px;
+    }
+  }
+
+  button {
+    cursor: pointer;
+    width: 25%;
+    height: 30px;
+    font-size: 1.8rem;
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    background: black;
+    color: white;
+    transition: all 200ms linear;
+    &:hover {
+      background: white;
+      color: black;
+    }
+`;
